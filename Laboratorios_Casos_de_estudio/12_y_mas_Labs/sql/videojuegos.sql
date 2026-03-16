f660f4f1b9306aa25c3589d7df790b35262d5654
amazingly145 <A01753419@tec.mx>
2026-03-15 19:08:10 -0600

    agregar tablas de roles y privilegios para RBAC

--- Laboratorios_Casos_de_estudio/12_y_mas_Labs/sql/videojuegos.sql
+++ Laboratorios_Casos_de_estudio/12_y_mas_Labs/sql/videojuegos.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 12, 2026 at 01:11 AM
+-- Generation Time: Mar 16, 2026 at 02:05 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -24,6 +24,54 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `otorga`
+--
+
+CREATE TABLE `otorga` (
+  `id_rol` int(11) NOT NULL,
+  `id_privilegio` int(11) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `privilegios`
+--
+
+CREATE TABLE `privilegios` (
+  `id` int(11) NOT NULL,
+  `privilegio` varchar(100) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `roles`
+--
+
+CREATE TABLE `roles` (
+  `id` int(11) NOT NULL,
+  `rol` varchar(100) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tiene`
+--
+
+CREATE TABLE `tiene` (
+  `id_usuario` varchar(50) NOT NULL,
+  `id_rol` int(11) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `users`
 --
 
@@ -31,10 +79,16 @@ CREATE TABLE `users` (
   `username` varchar(50) NOT NULL,
   `password` varchar(300) NOT NULL,
   `nombre` varchar(300) NOT NULL,
-  `created_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  `email` varchar(150) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
-ALTER TABLE `users` ADD `email` VARCHAR(150) NOT NULL AFTER `created_at`;
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`username`, `password`, `nombre`, `created_at`, `email`) VALUES
+('andy', '$2b$12$rkBFxQB6eD4e5i2fq1JT0uIdkX41EsiSuk5QcgKlBoSJdecsieHou', 'Andrea', '2026-03-15 22:49:42', 'acantumayorga@gmail.com');
 
 -- --------------------------------------------------------
 
@@ -50,8 +104,41 @@ CREATE TABLE `videojuegos` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
+-- Dumping data for table `videojuegos`
+--
+
+INSERT INTO `videojuegos` (`id`, `nombre`, `imagen`, `created_at`) VALUES
+(1, 'Snoopy: Strr', 'https://en.wikipedia.org/wiki/Snoopy%27s_Street_Fair', '2026-03-12 23:43:01');
+
+--
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `otorga`
+--
+ALTER TABLE `otorga`
+  ADD PRIMARY KEY (`id_rol`,`id_privilegio`),
+  ADD KEY `id_privilegio` (`id_privilegio`);
+
+--
+-- Indexes for table `privilegios`
+--
+ALTER TABLE `privilegios`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `roles`
+--
+ALTER TABLE `roles`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `tiene`
+--
+ALTER TABLE `tiene`
+  ADD PRIMARY KEY (`id_usuario`,`id_rol`),
+  ADD KEY `id_rol` (`id_rol`);
 
 --
 -- Indexes for table `users`
@@ -73,7 +160,25 @@ ALTER TABLE `videojuegos`
 -- AUTO_INCREMENT for table `videojuegos`
 --
 ALTER TABLE `videojuegos`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `otorga`
+--
+ALTER TABLE `otorga`
+  ADD CONSTRAINT `otorga_ibfk_1` FOREIGN KEY (`id_privilegio`) REFERENCES `privilegios` (`id`),
+  ADD CONSTRAINT `otorga_ibfk_2` FOREIGN KEY (`id_rol`) REFERENCES `roles` (`id`);
+
+--
+-- Constraints for table `tiene`
+--
+ALTER TABLE `tiene`
+  ADD CONSTRAINT `tiene_ibfk_1` FOREIGN KEY (`id_usuario`) REFERENCES `users` (`username`),
+  ADD CONSTRAINT `tiene_ibfk_2` FOREIGN KEY (`id_rol`) REFERENCES `roles` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
